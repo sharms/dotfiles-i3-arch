e8b2fd84a139737514e07b4cefdfc35c3e8d2d89
Steven Harms <sharms@snowfoundry.com>
2019-04-20 09:27:33 -0600

    Update

---
+++ packages.sh
@@ -0,0 +1,10 @@
+pacman -S code, compton-tryone-git, font-size, fzf, gnome-tweak, java-runtime, kubectl, kubern, lsof, perl, samba, -S code, -S cups cups-pdf, -S emacs, -S fzf, -S gnome, -S gnome-tweak-tool, -S jre-openjdk, -S lsof, -S os-prober, -S powerline-fonts, -S samba, -Ss gnome | less, -S steam, -Ss thunderbird, -S sway, -S system-config-printer, -S texlive-latexextra cups gummi, -S thunar, -S thunderbird, -S tumbler thunar-volman, sway, -S weechat, -Syu, texlive, texlive-latexextra cups gummi, topicon, urxvt, visualstudio
+yay -S autolock, chrome, compton, datagrip, font-size, foo2zjs-nightly, gcloud, gnome-shell-extension-topicons-plus, goland, golang, google-chrome, google-cloud-sdk, gtklp, hbij, hbl, hblij, hbpl, i3lock, i3lock-fancy-git, intellij, kubectl, kubectl-bin, powerline, rxvt, synergy, synergy2-bin, urxvt-font-size-git, xcursor-chameleon-pearl
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+systemctl enable pulseaudio
+
+git config --global user.email "sharms@snowfoundry.com"
+git config --global user.name "Steven Harms"
+
+
